e2e85dfc837ef37b6e2224a548477b7fb80e0282
Chris Kerbo <chris.kerbo@outlook.com>
2020-03-16 14:56:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solutions in this file
+const names = ['Chris', 'Eric', 'Michael'];
+const event = 'birthday';
+
+function writeCards(names, event) {
+  for (let i = 0, i < names.length; i++) {
+    console.log('Thank you, ${names[i]}, for the wonderful ${event} gift!')
+  };
+}
+  
