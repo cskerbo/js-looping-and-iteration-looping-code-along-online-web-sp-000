6b3ffabe3f04a15f0dd1a3b518f3db2e86b4629f
Chris Kerbo <chris.kerbo@outlook.com>
2020-03-16 15:07:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const event = 'birthday';
 
 function writeCards(names, event) {
   for (let i = 0; i < names.length; i++) {
-    console.log('Thank you, ${names[i]}, for the wonderful ${event} gift!');
+    console.log('Thank you, ${names}, for the wonderful ${event} gift!');
   }
